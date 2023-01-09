713b302ca00026856e3433ac58c59d7fb31398b0
Jota Feldmann <1572665+jotafeldmann@users.noreply.github.com>
2023-01-08 21:52:25 -0300

    feature: add key press detector

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 class CaptionControl {
-  constructor(captionElement = '.player-timedtext') {
+  constructor(captionElement = '.player-timedtext', shortcutSwitcherKey = 'c') {
     this._captionElement = captionElement
+    this._shortcutKey = shortcutSwitcherKey
     this._styleElement = null
+    this._captionIsVisible = true
     this._createStyleElement()
+    this._bindKeyPressDetector()
   }
   
   hide() {
     this._setCaptionDisplayStyle('none')
+    return false
   }
   
   show() {
     this._setCaptionDisplayStyle('block')
+    return true
+  }
+  
+  switchCaptionStatus() {
+    this._captionIsVisible = this._captionIsVisible ? this.hide() : this.show()
+  }
+  
+  _bindKeyPressDetector() {
+    document.onkeydown = function (e) {
+      if (e.key === this._shortcutKey) this.switchCaptionStatus()
+    }
   }
   
   _createStyleElement() {
@@ -23,3 +38,7 @@ class CaptionControl {
   }
   
 }
+
+var c = new CaptionControl()
+
+
